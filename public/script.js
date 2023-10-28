fb5dedbdf5f13019ade4790b1593c5ecf22c7f3a
Dox077 <149229368+Dox077@users.noreply.github.com>
2023-10-28 15:51:26 +0530

    t

--- public/script.js
+++ public/script.js
@@ -12,6 +12,7 @@ const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
 function handleClientLoad() {
   gapi.load('client:auth2', initClient);
 }
+
 function initClient() {
   gapi.client.init({
     clientId: CLIENT_ID,
@@ -29,44 +30,4 @@ function initClient() {
   });
 }
 
-function updateSigninStatus(isSignedIn) {
-  if (!isSignedIn) {
-    // User is not signed in, disable the "Store IP Address" button
-    document.getElementById('storeButton').disabled = true;
-  }
-}
-
-// Handle the click event of the "Store IP Address" button
-document.getElementById('storeButton').addEventListener('click', storeIpAddress);
-
-// Function to fetch IP address
-function storeIpAddress() {
-  fetch('https://api64.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-      const ipAddress = data.ip;
-      appendIpAddressToSheet(ipAddress);
-    })
-    .catch(error => {
-      console.error('Error fetching IP address:', error);
-    });
-}
-
-// Function to append the IP address to the Google Sheet
-function appendIpAddressToSheet(ipAddress) {
-  gapi.client.sheets.spreadsheets.values.append({
-    spreadsheetId: SPREADSHEET_ID,
-    range: 'Ip', // Update with your sheet name and range
-    valueInputOption: 'RAW',
-    insertDataOption: 'INSERT_ROWS',
-    resource: {
-      values: [[ipAddress]],
-    },
-  }).then(function (response) {
-    console.log('IP Address stored in Google Sheet:', response);
-    alert('IP Address stored in Google Sheet');
-  }).catch(function (error) {
-    console.error('Error storing IP address in Google Sheet:', error);
-    alert('Failed to store IP address in Google Sheet');
-  });
-}
+// ... The rest of your code
