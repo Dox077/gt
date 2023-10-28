5917658ea829db7324203d39386c3703717603e9
Dox077 <149229368+Dox077@users.noreply.github.com>
2023-10-28 16:06:38 +0530

    t

--- public/script.js
+++ public/script.js
@@ -1,61 +1,9 @@
-// Define your client ID (replace with your actual value)
-const CLIENT_ID = '1023049278699-489s2oqrr75au1a0e214hjvtqr99kfbk.apps.googleusercontent.com';
-const SPREADSHEET_ID = '1UDDarsUSFfdEhALQUlPvONklLpR0jzeV5kTaJ6Nslr4';
-
-
-const DISCOVERY_DOCS = ['https://sheets.googleapis.com/$discovery/rest?version=v4'];
-const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
-
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
-
-function initClient() {
-    gapi.client.init({
-        apiKey: CLIENT_ID,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-    }).then(function () {
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    });
-}
-
-function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-        document.getElementById('storeButton').disabled = false;
-    } else {
-        document.getElementById('storeButton').disabled = true;
-    }
-}
-
-function storeIpAddress() {
-    fetch('https://api64.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            const ipAddress = data.ip;
-            appendIpAddressToSheet(ipAddress);
-        })
-        .catch(error => {
-            console.error('Error fetching IP address:', error);
-        });
-}
-
-function appendIpAddressToSheet(ipAddress) {
-    gapi.client.sheets.spreadsheets.values.append({
-        spreadsheetId: SPREADSHEET_ID,
-        range: 'Ip',
-        valueInputOption: 'RAW',
-        insertDataOption: 'INSERT_ROWS',
-        resource: {
-            values: [[ipAddress]]
-        }
-    }).then(function (response) {
-        console.log('IP Address stored in Google Sheet:', response);
-        alert('IP Address stored in Google Sheet');
-    }).catch(function (error) {
-        console.error('Error storing IP address in Google Sheet:', error);
-        alert('Failed to store IP address in Google Sheet');
-    });
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('ipForm');
+  form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      fetch('/track-ip', {
+          method: 'POST'
+      });
+  });
+});
---
+++ public/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const publicIp = require('public-ip');
+
+const app = express();
+
+// Load your Google Sheets API credentials JSON here
+const credentials = require('./your-credentials.json');
+const doc = new GoogleSpreadsheet('YOUR_GOOGLE_SHEET_ID');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/track-ip', async (req, res) => {
+    try {
+        await doc.useServiceAccountAuth(credentials);
+        await doc.loadInfo();
+
+        const sheet = doc.sheetsByIndex[0]; // Assuming it's the first sheet.
+        const ipAddress = await publicIp.v4();
+        const currentTime = new Date().toLocaleString();
+
+        const newRow = {
+            IP_Address: ipAddress,
+            Timestamp: currentTime
+        };
+
+        await sheet.addRow(newRow);
+
+        console.log('IP address added to Google Sheet:', newRow);
+
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error occurred.');
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
