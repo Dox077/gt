a28850d0387a2bac77217f2ad5e941048b77ff97
Dox077 <149229368+Dox077@users.noreply.github.com>
2024-05-29 21:21:58 +0530

    c

--- public/script.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const form = document.getElementById('ipForm');
-//   form.addEventListener('submit', function (event) {
-//       event.preventDefault();
-//       fetch('/track-ip', {
-//           method: 'POST'
-//       });
-//   });
-// });
