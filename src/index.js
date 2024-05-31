a778de237f4b507e97f475aeb78b048cfe5caa4c
Dox077 <149229368+Dox077@users.noreply.github.com>
2024-05-31 23:34:09 +0530

    c

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import IpFetcher from '../components/IpFetcher';
+import IpFetcher from '../src/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <IpFetcher />
   </React.StrictMode>
 );
 
